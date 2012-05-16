f3630c9e3fcbab4063afde14de88ce5c530756fa
Steffen Schwigon <ss5@renormalist.net>
2012-05-16 11:23:57 +0200

    ABOUT section

--- lib/Scalar/Util/reftype/Inline.pm
+++ lib/Scalar/Util/reftype/Inline.pm
@@ -20,3 +20,16 @@ sub unimport {
 }
 
 1;
+
+=head1 ABOUT
+
+A highly experimental inlined C<reftype> (implemented by rafl++).
+Provided in the hope to be faster in special cases where reftype is a
+bottle-neck, like it is, for instance, sometimes in
+L<Data::DPath|Data::DPath>.
+
+It seems to not help as much as anticipated, but is provided as
+reference. (In the early days, when it still memleaked, it B<was>
+faster, too bad we have to collect garbage...)
+
+=cut
